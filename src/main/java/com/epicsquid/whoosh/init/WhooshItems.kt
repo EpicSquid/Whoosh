e93fa660df6f248ffbeaba651d8f4305f7793648
Grin <tyler.jefferies117@gmail.com>
2023-06-04 21:28:41 +1000

    wip creativemode tab
    Might have to take another look at the creative mode tab all together, added it seperatly

--- src/main/java/com/epicsquid/whoosh/init/WhooshItems.kt
+++ src/main/java/com/epicsquid/whoosh/init/WhooshItems.kt
@@ -6,23 +6,21 @@ import com.tterrag.registrate.util.entry.ItemEntry
 import com.tterrag.registrate.util.nullness.NonNullFunction
 import com.tterrag.registrate.util.nullness.NonNullSupplier
 import dev.epicsquid.whoosh.Whoosh
+import dev.epicsquid.whoosh.Whoosh.Companion.MODID
 import net.minecraft.world.item.CreativeModeTab
 import net.minecraft.world.item.Item
 
 object WhooshItems {
     private val REGISTRATE: Registrate = Whoosh.registrate
-    val TRANSPORTER: ItemEntry<TransporterItem> = REGISTRATE.item<TransporterItem>(
-        "transporter",
+    val TRANSPORTER: ItemEntry<TransporterItem> = REGISTRATE.item<TransporterItem>("transporter",
         NonNullFunction<Item.Properties, TransporterItem> { props: Item.Properties? ->
             TransporterItem(
-                props,
-                10000,
-                1000
+                props, 10000, 1000
             )
         })
         //add item to Creative mode tab
 
-        .tab(NonNullSupplier<CreativeModeTab> { CreativeModeTab(Whoosh)})
+        .tab(NonNullSupplier<CreativeModeTab> { CreativeModeTab(MODID)})
         .register()
 
     fun init() {}
