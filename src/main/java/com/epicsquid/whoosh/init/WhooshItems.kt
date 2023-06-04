81008b221f9bb9d2548c9e5dfd7a9ed9e5cb3341
Grin <tyler.jefferies117@gmail.com>
2023-06-04 21:07:15 +1000

    Converting to kotlin,
    importing nonsense

---
+++ src/main/java/com/epicsquid/whoosh/init/WhooshItems.kt
@@ -0,0 +1,29 @@
+package com.epicsquid.whoosh.init
+
+import com.epicsquid.whoosh.item.TransporterItem
+import com.tterrag.registrate.Registrate
+import com.tterrag.registrate.util.entry.ItemEntry
+import com.tterrag.registrate.util.nullness.NonNullFunction
+import com.tterrag.registrate.util.nullness.NonNullSupplier
+import dev.epicsquid.whoosh.Whoosh
+import net.minecraft.world.item.CreativeModeTab
+import net.minecraft.world.item.Item
+
+object WhooshItems {
+    private val REGISTRATE: Registrate = Whoosh.registrate
+    val TRANSPORTER: ItemEntry<TransporterItem> = REGISTRATE.item<TransporterItem>(
+        "transporter",
+        NonNullFunction<Item.Properties, TransporterItem> { props: Item.Properties? ->
+            TransporterItem(
+                props,
+                10000,
+                1000
+            )
+        })
+        //add item to Creative mode tab
+
+        .tab(NonNullSupplier<CreativeModeTab> { CreativeModeTab(Whoosh)})
+        .register()
+
+    fun init() {}
+}
---
+++ src/main/java/com/epicsquid/whoosh/init/WhooshMenuTypes.kt
@@ -0,0 +1,37 @@
+package com.epicsquid.whoosh.init
+
+import com.epicsquid.whoosh.containers.TransporterMenu
+import com.epicsquid.whoosh.gui.TransportScreen
+import com.tterrag.registrate.Registrate
+import com.tterrag.registrate.builders.MenuBuilder.ScreenFactory
+import dev.epicsquid.whoosh.Whoosh
+import net.minecraft.network.FriendlyByteBuf
+import net.minecraft.network.chat.Component
+import net.minecraft.world.entity.player.Inventory
+import net.minecraft.world.inventory.MenuType
+
+object WhooshMenuTypes {
+    private val REGISTRATE: Registrate = Whoosh.registrate
+    @JvmField
+	val TRANSPORTER = REGISTRATE.menu<TransporterMenu, TransportScreen>(
+        "transporter",
+        { type: MenuType<TransporterMenu?>?, id: Int, inventory: Inventory?, buf: FriendlyByteBuf? ->
+            TransporterMenu(
+                type,
+                id,
+                inventory,
+                buf
+            )
+        }) {
+        ScreenFactory { container: TransporterMenu?, inv: Inventory?, component: Component? ->
+            TransportScreen(
+                container,
+                inv,
+                component
+            )
+        }
+    }
+        .register()
+
+    fun init() {}
+}
