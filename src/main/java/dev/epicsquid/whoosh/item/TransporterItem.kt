005216d5df7e16a0cd47f6f35f1d7963fd6f1907
Grin <tyler.jefferies117@gmail.com>
2023-06-05 15:37:18 +1000

    Trying to get the menu to open or change mode
    Also toml fix  from thermalenedgy
    Blink mode works, just forgot to give it power rip,

--- src/main/java/dev/epicsquid/whoosh/item/TransporterItem.kt
+++ src/main/java/dev/epicsquid/whoosh/item/TransporterItem.kt
@@ -25,77 +25,69 @@ import net.minecraft.world.inventory.AbstractContainerMenu
 import net.minecraft.world.item.ItemStack
 import net.minecraft.world.level.Level
 import net.minecraftforge.network.NetworkHooks
+import net.minecraftforge.network.NetworkHooks.openScreen
 import java.util.function.IntSupplier
 import javax.annotation.Nonnull
 
 class TransporterItem(builder: Properties?, maxEnergy: Int, maxTransfer: Int) :
-    EnergyContainerItemAugmentable(builder, maxEnergy, maxTransfer), IFlexibleEnergyContainerItem, IMultiModeItem,
-    MenuProvider {
-    init {
-        numSlots = IntSupplier { 4 }
-        augValidator = ThermalAugmentRules.createAllowValidator(
-            NBTTags.TAG_AUGMENT_TYPE_UPGRADE,
-            NBTTags.TAG_AUGMENT_TYPE_RF,
-            NBTTags.TAG_AUGMENT_TYPE_AREA_EFFECT
-        )
-    }
+	EnergyContainerItemAugmentable(builder, maxEnergy, maxTransfer), IFlexibleEnergyContainerItem, IMultiModeItem,
+	MenuProvider {
+	init {
+		numSlots = IntSupplier { 4 }
+		augValidator = ThermalAugmentRules.createAllowValidator(
+			NBTTags.TAG_AUGMENT_TYPE_UPGRADE, NBTTags.TAG_AUGMENT_TYPE_RF, NBTTags.TAG_AUGMENT_TYPE_AREA_EFFECT
+		)
+	}
 
-    @Nonnull
-    override fun use(
-        @Nonnull level: Level,
-        @Nonnull player: Player,
-        @Nonnull hand: InteractionHand
-    ): InteractionResultHolder<ItemStack> {
-        val stack = player.getItemInHand(hand)
-        if (Utils.isFakePlayer(player) || hand == InteractionHand.OFF_HAND) {
-            return InteractionResultHolder.fail(stack)
-        }
-        if (player is ServerPlayer) {
-            if (getMode(stack) == BLINK_MODE) {
-                // Check energy can be consumed
-                if (extractEnergy(stack, 400, true) >= 400) {
-                    if (TeleportUtils.shortTeleport(level, player, hand)) {
-                        // Consume energy
-                        extractEnergy(stack, 400, false)
-                        return InteractionResultHolder(InteractionResult.SUCCESS, stack)
-                    }
-                }
-            } else if (getMode(stack) == TELEPORT_MODE) {
-                NetworkHooks.openScreen(player, this)
-            }
-        }
-        return InteractionResultHolder.success(stack)
-    }
+	//what happens when player uses item
+	override fun use(
+		@Nonnull level: Level, @Nonnull player: Player, @Nonnull hand: InteractionHand
+	): InteractionResultHolder<ItemStack> {
+		val stack = player.getItemInHand(hand)
+		if (Utils.isFakePlayer(player) || hand == InteractionHand.OFF_HAND) {
+			return InteractionResultHolder.fail(stack)
+		}
+		if (player is ServerPlayer) {
+			if (getMode(stack) == BLINK_MODE) {
+				// Check energy can be consumed
+				if (extractEnergy(stack, 400, true) >= 400) {
+					if (TeleportUtils.shortTeleport(level, player, hand)) {
+						// Consume energy
+						extractEnergy(stack, 400, false)
+						return InteractionResultHolder(InteractionResult.SUCCESS, stack)
+					}
+				}
+			} else if (getMode(stack) == TELEPORT_MODE) {
+				openScreen(player, this)
+			}
+		}
+		return InteractionResultHolder.success(stack)
+	}
 
-    override fun getDisplayName(): Component {
-        return WhooshLang.TRANSPORTER
-    }
+	override fun getDisplayName(): Component {
+		return WhooshLang.TRANSPORTER
+	}
 
-    override fun createMenu(
-        id: Int,
-        playerInv: @NonnullType Inventory?,
-        player: @NonnullType Player?
-    ): AbstractContainerMenu? {
-        return TransporterMenu(WhooshMenuTypes.TRANSPORTER.get(), id, playerInv, player)
-    }
+	override fun createMenu(
+		id: Int, playerInv: @NonnullType Inventory?, player: @NonnullType Player?
+	): AbstractContainerMenu {
+		return TransporterMenu(WhooshMenuTypes.TRANSPORTER.get(), id, playerInv, player)
+	}
 
-    override fun onModeChange(player: Player, stack: ItemStack) {
-        player.level.playSound(
-            null,
-            player.blockPosition(),
-            SoundEvents.ITEM_PICKUP,
-            SoundSource.PLAYERS,
-            0.4f,
-            0.8f + 0.4f * getMode(stack)
-        )
-//        ChatHelper.sendIndexedChatMessageToPlayer(
-//            player,
-//            if (getMode(stack) == BLINK_MODE) WhooshLang.MODE_BLINK else WhooshLang.MODE_TELEPORT
-//        )
-    }
+	override fun onModeChange(player: Player, stack: ItemStack) {
+		player.level.playSound(
+			null,
+			player.blockPosition(),
+			SoundEvents.ITEM_PICKUP,
+			SoundSource.PLAYERS,
+			0.4f,
+			0.8f + 0.4f * getMode(stack)
+		)
+		if (getMode(stack) == BLINK_MODE) WhooshLang.MODE_BLINK else WhooshLang.MODE_TELEPORT
+	}
 
-    companion object {
-        private const val BLINK_MODE = 0
-        private const val TELEPORT_MODE = 1
-    }
+	companion object {
+		private const val BLINK_MODE = 0
+		private const val TELEPORT_MODE = 1
+	}
 }
