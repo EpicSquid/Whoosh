847b297034735e5fca14b207b458be06eef7e915
Grin <tyler.jefferies117@gmail.com>
2023-06-08 15:22:24 +1000

    Creative tab and resistrate

--- src/main/java/dev/epicsquid/whoosh/Whoosh.kt
+++ src/main/java/dev/epicsquid/whoosh/Whoosh.kt
@@ -1,6 +1,7 @@
 package dev.epicsquid.whoosh
 
 import com.mojang.logging.LogUtils
+import com.tterrag.registrate.Registrate
 import dev.epicsquid.whoosh.registery.WhooshCaps
 import dev.epicsquid.whoosh.registery.WhooshItems
 import dev.epicsquid.whoosh.registery.WhooshLang
@@ -21,63 +22,69 @@ import net.minecraftforge.registries.ForgeRegistries
 
 @Mod(Whoosh.MODID)
 class Whoosh {
-	init {
-		val modEventBus = FMLJavaModLoadingContext.get().modEventBus
 
-		// Register the commonSetup method for modloading
-		modEventBus.addListener { event: FMLCommonSetupEvent ->
-			commonSetup(
-				event
-			)
+
+		init {
+				val modEventBus = FMLJavaModLoadingContext.get().modEventBus
+
+				// Register the commonSetup method for modloading
+				modEventBus.addListener { event: FMLCommonSetupEvent ->
+						commonSetup(
+								event
+						)
+				}
+
+				// Register the Deferred Register to the mod event bus so items get registered
+				ITEMS.register(modEventBus)
+				WhooshItems.init()
+				WhooshCaps.init()
+				WhooshLang.init()
+				WhooshMenuTypes.init()
+
+				// Register ourselves for server and other game events we are interested in
+				MinecraftForge.EVENT_BUS.register(this)
 		}
 
-		// Register the Deferred Register to the mod event bus so items get registered
-		ITEMS.register(modEventBus)
-			WhooshItems.init()
-			WhooshCaps.init()
-			WhooshLang.init()
-			WhooshMenuTypes.init()
+		private fun commonSetup(event: FMLCommonSetupEvent) {
+				// Some common setup code
+				LOGGER.info("HELLO FROM COMMON SETUP")
+				LOGGER.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.DIRT))
+		}
 
-		// Register ourselves for server and other game events we are interested in
-		MinecraftForge.EVENT_BUS.register(this)
-	}
-
-	private fun commonSetup(event: FMLCommonSetupEvent) {
-		// Some common setup code
-		LOGGER.info("HELLO FROM COMMON SETUP")
-		LOGGER.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.DIRT))
-	}
-
-	// You can use SubscribeEvent and let the Event Bus discover methods to call
-	@SubscribeEvent
-	fun onServerStarting(event: ServerStartingEvent?) {
-		// Do something when the server starts
-		LOGGER.info("HELLO from server starting")
-	}
-
-	// You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-	@EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.MOD, value = [Dist.CLIENT])
-	object ClientModEvents {
+		// You can use SubscribeEvent and let the Event Bus discover methods to call
 		@SubscribeEvent
-		fun onClientSetup(event: FMLClientSetupEvent?) {
-			// Some client setup code
-			LOGGER.info("HELLO FROM CLIENT SETUP")
-			LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().user.name)
+		fun onServerStarting(event: ServerStartingEvent?) {
+				// Do something when the server starts
+				LOGGER.info("HELLO from server starting")
 		}
-	}
 
-	companion object {
-		// Define mod id in a common place for everything to reference
-		const val MODID = "whoosh"
+		// You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+		@EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.MOD, value = [Dist.CLIENT])
+		object ClientModEvents {
+				@SubscribeEvent
+				fun onClientSetup(event: FMLClientSetupEvent?) {
+						// Some client setup code
+						LOGGER.info("HELLO FROM CLIENT SETUP")
+						LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().user.name)
+				}
+		}
 
-		// Directly reference a slf4j logger
-		private val LOGGER = LogUtils.getLogger()
+		companion object {
+				// Define mod id in a common place for everything to reference
+				const val MODID = "whoosh"
+				val registrate by lazy { Registrate.create(MODID).creativeModeTab { tab } }
 
 
-		// Create a Deferred Register to hold Items which will all be registered under the "whoosh" namespace
-		val ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID)
+				// Directly reference a slf4j logger
+				private val LOGGER = LogUtils.getLogger()
 
-	}
+
+				// Create a Deferred Register to hold Items which will all be registered under the "whoosh" namespace
+				val ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID)
+
+		}
+
 }
+
 
 
